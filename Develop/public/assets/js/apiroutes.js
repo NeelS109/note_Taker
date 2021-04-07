14146e299f4f51ef195cd516862c9e5c8fde4efc
Neel Sheth <nsheth109@gmail.com>
2021-04-07 17:02:11 -0400

    posted and get on so we can create new note in app

--- Develop/public/assets/js/apiroutes.js
+++ Develop/public/assets/js/apiroutes.js
@@ -24,7 +24,6 @@ module.exports = (app) => {
                 console.log(error);
                 return;
             }
-            
             for (let [i, item] of note.submit()) {
                 if (item.id === req.params.id) {
                     note.splice(i, 1);
@@ -38,3 +37,27 @@ module.exports = (app) => {
             }
         })
     })
+
+    app.post('/api/notes/', (req, res) => {
+        var newNote = {
+            "title": req.body.title,
+            "text": req.body.text,
+            "id": uuidv1()
+        };
+        databaseFile.push(newNote);
+        fs.writeFile("../../../db/db.json", JSON.stringify(databaseFile), error => {
+            if (error) throw error;
+            console.log("Finished Writing")
+        })
+    })
+
+    app.get('/api/notes', (req, res) => {
+        jsonReader('../../../db/db.json', (error, note) => {
+            if (error) {
+                console.log(error);
+                return;
+            }
+            res.send(note);
+        })
+    })
+}
